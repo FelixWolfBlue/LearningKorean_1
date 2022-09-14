16958f44feea0925682088a6b2c71e5b70c1da93
FelixWolfBlue <97985344+FelixWolfBlue@users.noreply.github.com>
2022-09-14 17:13:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-currentDate = new Date();
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-dateToPrint = currentDate.getFullYear() + " " + months[currentDate.getMonth()] + " " + currentDate.getDate();
+document.getElementById('todayDate').innerHTML = "test";
 
+document.getElementById('theOtherDate').innerHTML = "testAgain";
 
-document.getElementById('answerBox').innerHTML = dateToPrint; 
+otherDate = new Date(getRandomInt(2022),getRandomInt(11),getRandomInt(31),getRandomInt(12),getRandomInt(60));
+
+stringOtherDate = otherDate.getYear() + " " + months[otherDate.getMonth()] + " " + otherDate.getDate() + " " + otherDate.getHours() + ":" + otherDate.getMinutes();
+
+document.getElementById('theOtherDate').innerHTML = stringOtherDate;
+
